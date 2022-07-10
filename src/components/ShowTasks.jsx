561703f01ba3d5df12a5ca237813dbfb3180d72c
selcuklu <yilmax4@gmail.com>
2022-07-10 22:25:49 +0300

    checkMark

--- src/components/ShowTasks.jsx
+++ src/components/ShowTasks.jsx
@@ -1,20 +1,38 @@
 import { MdClear } from "react-icons/md";
 import { FcCheckmark } from "react-icons/fc";
+// import { useState } from "react";
 
 const ShowTasks = ({ tasks, setTasks }) => {
+// const [info, setİnfo] = useState({isDone:true})
 
+const line=(id)=>{
+  setTasks(tasks.map((tsk)=>{
+    if(tsk.id===id){
+      return{...tsk,isDone:!tsk.isDone}
+    }else{
+      return tsk
+    }
+  }))
+  // const newline={isDone:false}
+  // setİnfo(...info,newline)
+}
+  
   return (
     <div>
       {tasks.map((item) => {
         return (
           <ul key={item.id}>
-            <li className={item.isDone ? "inva text-decoration-line-through ": "inva" }>
+            <li className={item.isDone ? "inva text-decoration-line-through" : "inva" }>
               <FcCheckmark
-              onClick={()=>{setTasks(
-                tasks.map((task) =>
-                  task.id === item.id ? { ...item, isDone: true } : item)
-              )
-            }}
+              onClick={()=>line(item.id)}
+              // onClick={()=>{setTasks(
+              //   tasks
+              //     .filter((task) => task.id === item.id)
+              //     .map(() => ({ ...item, isDone: !item.isDone }))
+                // .map((tsk) =>
+                //   tsk.id === item.id ? { ...item, isDone: !item.isDone } : item)
+            //   );
+            // }}
               />
                {/* {console.log(item.isDone)} */}
               {item.task} <br /> 
@@ -25,9 +43,12 @@ const ShowTasks = ({ tasks, setTasks }) => {
                 className="mdclear"
               />{item.date}
             </li>
+
           </ul>
+         
         );
       })}
+       <p>{console.log(tasks)}</p> 
     </div>
   );
 };
